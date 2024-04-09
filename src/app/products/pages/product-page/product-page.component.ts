20807fcddf29ecca7683cde0ff40f1e9dcdad230
Abraham Gutierrez <labraham1986@gmail.com>
2024-04-08 22:01:51 -0500

    feat: add custom directive

---
+++ src/app/products/pages/product-page/product-page.component.ts
@@ -0,0 +1,22 @@
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  templateUrl: './product-page.component.html',
+  styles: ``
+})
+export class ProductPageComponent {
+
+  private fb = inject( FormBuilder );
+
+  public color: string = 'green';
+
+  public myform: FormGroup = this.fb.group({
+    name: ['', [ Validators.required, Validators.minLength(6), Validators.email ]]
+  });
+
+  changeColor(): void{
+    this.color = '#xxxxxx'.replace(/x/g, y=>(Math.random()*16|0).toString(16));
+  }
+
+}
