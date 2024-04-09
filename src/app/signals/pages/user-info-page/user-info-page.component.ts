c1a075d99d720b524a50b7e443aef7bb7c966c73
Abraham Gutierrez <labraham1986@gmail.com>
2024-04-09 17:32:05 -0500

    feat: add signals

---
+++ src/app/signals/pages/user-info-page/user-info-page.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit, computed, inject, signal } from '@angular/core';
+import { UsersService } from '../../services/users-service.service';
+import { User } from '../../interfaces/user-request.interface';
+
+@Component({
+  selector: 'app-user-info-page',
+  templateUrl: './user-info-page.component.html',
+  styles: ``
+})
+export class UserInfoPageComponent implements OnInit {
+
+  private UsersService = inject(UsersService);
+  public userId = signal(1);
+
+  public currentUser = signal<User | undefined>(undefined);
+  public userWasFound = signal(true);
+
+  public fullName = computed<string>(() => {
+    if (!this.currentUser) return 'Usuario no encontrado';
+    return `${this.currentUser()?.first_name} ${this.currentUser()?.last_name}`;
+  });
+
+
+  ngOnInit(): void {
+    this.loadUser(this.userId());
+  }
+
+  loadUser(id: number) {
+
+    if (id <= 0) return;
+
+    this.userId.set(id);
+    this.currentUser.set(undefined);
+
+    this.UsersService.getUserById(id)
+      .subscribe({
+        next: (value) => {
+          this.currentUser.set(value);
+          this.userWasFound.set(true);
+        },
+        error: () => {
+          this.userWasFound.set(false);
+          this.currentUser.set(undefined);
+        },
+
+      }
+      );
+
+  }
+
+}
